2cd0df5c28efb7340a851d6af66b8f4f64b1d83f
dhapdhap123 <dhapdhap123@naver.com>
2022-11-08 23:55:28 +0900

    1108

--- src/pages/WritePage.js
+++ src/pages/WritePage.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import Responsive from '../components/common/Responsive';
+import EditorContainer from '../components/write/EditorContainer';
+import TagBoxContainer from '../components/write/TagBoxContainer';
+import WriteActionButtonsContainer from '../components/write/WriteActionButtonsContainer';
 
 const WritePage = () => {
-  return <div>글쓰기</div>;
+  return (
+    <Responsive>
+      <EditorContainer />
+      <TagBoxContainer />
+      <WriteActionButtonsContainer />
+    </Responsive>
+  );
 };
 
 export default WritePage;
